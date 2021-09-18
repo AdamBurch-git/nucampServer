6945acd353d9545e2ee19d5f94317084c4f66763
Adam Burch <adamburchpta@gmail.com>
2021-09-18 14:10:26 -0500

    Week 4

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/nucampsite",
-    "facebook": {
-        clientId: "243954520980988",
-        clientSecret: "4852b1867665d0130beace3cd8618393"
-    }
-}
